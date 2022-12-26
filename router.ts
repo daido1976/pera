810f784c9a58d18cd366541745b16bbfb1efb543
daido1976 <daido1976@gmail.com>
2022-12-26 17:48:19 +0900

    Replace Routes

--- router.ts
+++ router.ts
@@ -5,8 +5,7 @@ import { staticHandler } from "./static.ts";
 const methods = ["GET", "POST", "PUT", "PATCH", "DELETE"] as const;
 const defaultMethod = methods[0];
 type Method = typeof methods[number];
-type Routes = Map<Method, { [path: string]: MicroHandler | undefined }>;
-type Routes2 = Map<
+type Routes = Map<
   Method,
   {
     path: string;
@@ -17,42 +16,21 @@ export type MicroHandler = (req: MicroRequest, res: MicroResponse) => Response;
 
 export class Router {
   #routes: Routes;
-  #routes2: Routes2;
 
   constructor() {
     this.#routes = new Map();
-    this.#routes2 = new Map();
   }
 
   register(method: Method, path: string, handler: MicroHandler) {
-    const current = this.#routes.get(method);
-    this.#routes.set(method, { ...current, [path]: handler });
-  }
-
-  register2(method: Method, path: string, handler: MicroHandler) {
-    const current = this.#routes2.get(method) ?? [];
-    this.#routes2.set(method, [...current, { path, handler }]);
+    const current = this.#routes.get(method) ?? [];
+    this.#routes.set(method, [...current, { path, handler }]);
   }
 
   resolve(req: MicroRequest, res: MicroResponse): Response {
     console.debug("[DEBUG] routes: ", this.#routes);
     const { pathname: path } = new URL(req.url);
     const method = this.#toMethod(req.method);
-    const pathRouter = this.#routes.get(method);
-
-    if (!pathRouter) {
-      return staticHandler(req, res);
-    }
-
-    const handler = pathRouter[path];
-    return handler ? handler(req, res) : staticHandler(req, res);
-  }
-
-  resolve2(req: MicroRequest, res: MicroResponse): Response {
-    console.debug("[DEBUG] routes: ", this.#routes);
-    const { pathname: path } = new URL(req.url);
-    const method = this.#toMethod(req.method);
-    const paths = this.#routes2.get(method) ?? [];
+    const paths = this.#routes.get(method) ?? [];
 
     if (paths.length === 0) {
       return staticHandler(req, res);

6b37fb6f5607b09d351df44cf14f9d52694dcd90
daido1976 <daido1976@gmail.com>
2022-12-28 08:02:48 +0900

    $ udd deps.ts

--- deps.ts
+++ deps.ts
@@ -1,7 +1,7 @@
-export { serve } from "https://deno.land/std@0.146.0/http/server.ts";
-export { format } from "https://deno.land/std@0.146.0/datetime/mod.ts";
+export { serve } from "https://deno.land/std@0.170.0/http/server.ts";
+export { format } from "https://deno.land/std@0.170.0/datetime/mod.ts";
 export {
   join,
   extname,
   basename,
-} from "https://deno.land/std@0.167.0/path/mod.ts";
+} from "https://deno.land/std@0.170.0/path/mod.ts";

c70754edf46aeb7856f3daae89913554cc71f99f
daido1976 <daido1976@gmail.com>
2023-01-21 19:07:23 +0900

    Use serveFile from the deno standard library

--- static.ts
+++ static.ts
@@ -1,23 +1,11 @@
-import { basename, extname, join } from "./deps.ts";
+import { basename, join } from "./deps.ts";
 import { PeraHandler } from "./router.ts";
-
-// TODO: refactor with https://deno.land/std@0.170.0/media_types/mod.ts
-const MIME_TYPES: { [key: string]: string } = {
-  default: "application/octet-stream",
-  html: "text/html; charset=UTF-8",
-  js: "application/javascript; charset=UTF-8",
-  css: "text/css",
-  png: "image/png",
-  jpg: "image/jpg",
-  gif: "image/gif",
-  ico: "image/x-icon",
-  svg: "image/svg+xml",
-};
+import { serveFile } from "https://deno.land/std@0.173.0/http/file_server.ts";
 
 const STATIC_PATH = "./public";
 
 // TODO: http://localhost:8000/path/index.css にアクセスした時に not found にする（= /index.css の時しか返さない）
-export const staticHandler: PeraHandler = (req, res) => {
+export const staticHandler: PeraHandler = (req) => {
   // TODO: If it is not GET method, return early.
   const toLocalPath = (path: string) => {
     const fileName = basename(path);
@@ -26,25 +14,7 @@ export const staticHandler: PeraHandler = (req, res) => {
     return join(...paths);
   };
 
-  const toExt = (p: string) => extname(p).substring(1).toLowerCase();
-
   const { pathname } = new URL(req.url);
   const path = toLocalPath(pathname);
-  let data: Uint8Array;
-  try {
-    // TODO: to async
-    data = Deno.readFileSync(path);
-  } catch (_e: unknown) {
-    const notFound = res.status(404).text("not found");
-    return notFound;
-  }
-
-  // TOOD: to type safety
-  const mimeType = MIME_TYPES[toExt(path)] ?? MIME_TYPES.default;
-  return new Response(data, {
-    headers: {
-      "Content-Type": mimeType,
-    },
-    status: 200,
-  });
+  return serveFile(req, path);
 };

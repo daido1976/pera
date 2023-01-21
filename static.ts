cbf0f30df9fdbf29d249205b2c4b3e2159750279
daido1976 <daido1976@gmail.com>
2023-01-21 21:23:05 +0900

    Use serveDir from the deno standard library

--- static.ts
+++ static.ts
@@ -1,20 +1,8 @@
-import { basename, join } from "./deps.ts";
 import { PeraHandler } from "./router.ts";
-import { serveFile } from "https://deno.land/std@0.173.0/http/file_server.ts";
+import { serveDir } from "https://deno.land/std@0.173.0/http/file_server.ts";
 
 const STATIC_PATH = "./public";
 
-// TODO: http://localhost:8000/path/index.css にアクセスした時に not found にする（= /index.css の時しか返さない）
 export const staticHandler: PeraHandler = (req) => {
-  // TODO: If it is not GET method, return early.
-  const toLocalPath = (path: string) => {
-    const fileName = basename(path);
-    const paths = [STATIC_PATH, fileName];
-    if (path === "/") paths.push("index.html");
-    return join(...paths);
-  };
-
-  const { pathname } = new URL(req.url);
-  const path = toLocalPath(pathname);
-  return serveFile(req, path);
+  return serveDir(req, { fsRoot: STATIC_PATH, quiet: true });
 };

c74147cac17cbdf825e97e4145fe97db24ba666d
daido1976 <daido1976@gmail.com>
2023-01-22 22:51:16 +0900

    Fix type of query in Request

--- src/request.ts
+++ src/request.ts
@@ -1,6 +1,6 @@
 export class PeraRequest extends Request {
   params: { [key: string]: string } = {};
-  query: { [key: string]: string | string[] } = {};
+  query: { [key: string]: string } = {};
 
   constructor(rawReq: Request) {
     super(rawReq);

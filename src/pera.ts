4f3a1251c9827281d5c8f0e6dc7398e8ed2eb2a4
daido1976 <daido1976@gmail.com>
2023-02-22 21:36:01 +0900

    Regenerate jsdoc

--- src/pera.ts
+++ src/pera.ts
@@ -4,92 +4,93 @@ import { PeraHandler, Router } from "./router.ts";
 
 // NOTE: Supports only commonly used HTTP methods.
 // https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods
-/**
- * Pera is a simple web framework that allows you to register handlers for various HTTP methods and serves them.
- * @class
- */
 export class Pera {
-  /**
-   * Router instance for routing incoming HTTP requests.
-   * @private
-   */
   #router: Router = new Router();
 
   /**
-   * Register a handler function for HTTP GET requests to the specified path.
-   * @param {string} path - Path to register the handler for.
-   * @param {PeraHandler} handler - Handler function to register.
+   * Adds a GET request route to the router with the specified path and handler function.
+   *
+   * @param path - The path to register the GET request.
+   * @param handler - The function to handle the GET request.
    */
   get(path: string, handler: PeraHandler) {
     this.#router.register("GET", path, handler);
   }
 
   /**
-   * Register a handler function for HTTP POST requests to the specified path.
-   * @param {string} path - Path to register the handler for.
-   * @param {PeraHandler} handler - Handler function to register.
+   * Adds a POST request route to the router with the specified path and handler function.
+   *
+   * @param path - The path to register the POST request.
+   * @param handler - The function to handle the POST request.
    */
   post(path: string, handler: PeraHandler) {
     this.#router.register("POST", path, handler);
   }
 
   /**
-   * Register a handler function for HTTP PUT requests to the specified path.
-   * @param {string} path - Path to register the handler for.
-   * @param {PeraHandler} handler - Handler function to register.
+   * Adds a PUT request route to the router with the specified path and handler function.
+   *
+   * @param path - The path to register the PUT request.
+   * @param handler - The function to handle the PUT request.
    */
   put(path: string, handler: PeraHandler) {
     this.#router.register("PUT", path, handler);
   }
 
   /**
-   * Register a handler function for HTTP PATCH requests to the specified path.
-   * @param {string} path - Path to register the handler for.
-   * @param {PeraHandler} handler - Handler function to register.
+   * Adds a PATCH request route to the router with the specified path and handler function.
+   *
+   * @param path - The path to register the PATCH request.
+   * @param handler - The function to handle the PATCH request.
    */
   patch(path: string, handler: PeraHandler) {
     this.#router.register("PATCH", path, handler);
   }
 
   /**
-   * Register a handler function for HTTP DELETE requests to the specified path.
-   * @param {string} path - Path to register the handler for.
-   * @param {PeraHandler} handler - Handler function to register.
+   * Adds a DELETE request route to the router with the specified path and handler function.
+   *
+   * @param path - The path to register the DELETE request.
+   * @param handler - The function to handle the DELETE request.
    */
   delete(path: string, handler: PeraHandler) {
     this.#router.register("DELETE", path, handler);
   }
 
   /**
-   * Register a handler function for HTTP HEAD requests to the specified path.
-   * @param {string} path - Path to register the handler for.
-   * @param {PeraHandler} handler - Handler function to register.
+   * Adds a HEAD request route to the router with the specified path and handler function.
+   *
+   * @param path - The path to register the HEAD request.
+   * @param handler - The function to handle the HEAD request.
    */
   head(path: string, handler: PeraHandler) {
     this.#router.register("HEAD", path, handler);
   }
 
   /**
-   * Register a handler function for HTTP OPTIONS requests to the specified path.
-   * @param {string} path - Path to register the handler for.
-   * @param {PeraHandler} handler - Handler function to register.
+   * Adds an OPTIONS request route to the router with the specified path and handler function.
+   *
+   * @param path - The path to register the OPTIONS request.
+   * @param handler - The function to handle the OPTIONS request.
    */
   options(path: string, handler: PeraHandler) {
     this.#router.register("OPTIONS", path, handler);
   }
 
   /**
-   * Set custom static path for serving static files.
-   * @param {string} path - Path to serve static files from.
+   * Sets a custom static file path.
+   *
+   * @param path - The path to the custom static file.
    */
   static(path: string) {
     this.#router.setCustomStatic(path);
   }
 
   /**
-   * Handler function for incoming HTTP requests that delegates requests to the appropriate registered handler function.
-   * @param {Request} req - The incoming HTTP request object.
-   * @returns {Response|Promise<Response>} The HTTP response object or a promise that resolves to it.
+   * Handles the request and returns a response object or a promise that resolves to a response object.
+   *
+   * @param req - The request object.
+   * @returns The response object or a promise that resolves to a response object.
    */
   handler = (req: Request): Response | Promise<Response> => {
     const res = this.#router.resolve(req);

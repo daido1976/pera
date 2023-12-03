73ba0c57d33c88b2ad891d8632df01955096b735
daido1976 <daido1976@gmail.com>
2023-12-03 10:01:12 +0900

    $ deno task update:deps

--- src/deps.ts
+++ src/deps.ts
@@ -1,5 +1,5 @@
-export { format } from "https://deno.land/std@0.204.0/datetime/mod.ts";
-export { serveDir } from "https://deno.land/std@0.204.0/http/file_server.ts";
-export { describe, it } from "https://deno.land/std@0.204.0/testing/bdd.ts";
-export { assertEquals } from "https://deno.land/std@0.204.0/testing/asserts.ts";
-export { fromFileUrl } from "https://deno.land/std@0.204.0/path/mod.ts";
+export { format } from "https://deno.land/std@0.208.0/datetime/mod.ts";
+export { serveDir } from "https://deno.land/std@0.208.0/http/file_server.ts";
+export { describe, it } from "https://deno.land/std@0.208.0/testing/bdd.ts";
+export { assertEquals } from "https://deno.land/std@0.208.0/testing/asserts.ts";
+export { fromFileUrl } from "https://deno.land/std@0.208.0/path/mod.ts";
